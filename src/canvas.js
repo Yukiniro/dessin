682f0095f2251a4e4ab4346fe626911ca2579f9e
Yukiniro <yukiniro@hotmail.com>
2021-01-12 20:57:47 +0800

    加入事件视图绑定

--- src/canvas.js
+++ src/canvas.js
@@ -1,14 +1,17 @@
 import util from './util/util'
 import collection from './mixin/collection.mixin'
 import observable from './mixin/observable.mixin'
+import event from './event'
 
 class Canvas {
 
   constructor(props = {}) {
-    this.lowerCanvas = props && props.lowerCanvas; // 渲染视图
-    this.upperCanvas = props && props.upperCanvas; // 事件响应、辅助线视图
-    this.size = {width: 1, height: 1};
-    this.viewResponse = 1; // 实际渲染与最终成像之间的比例关系
+    this._lowerCanvas = props && props.lowerCanvas; // 渲染视图
+    this._upperCanvas = props && props.upperCanvas; // 事件响应、辅助线视图
+    this._size = {width: 1, height: 1};
+    this._viewResponse = 1; // 实际渲染与最终成像之间的比例关系
+
+    event.bindView(this._lowerCanvas, this._upperCanvas);
   }
 }
 
--- src/event.js
+++ src/event.js
@@ -1,4 +1,61 @@
-export default (function () {
+import constant from "./constant/constant";
+import util from "./util/util";
 
-  
+export default (function() {
+
+  let _view = null;
+  let _owner = null;
+  let _isMouseDown = false;
+
+  function onMouseDown(e) {
+    _isMouseDown = true;
+    _owner.fireEvent(constant.MOUSE_DOWN, e);
+    util.addEventListener(document.body, 'mouseup', onMouseUp);
+  }
+
+  function onMouseMove(e) {
+    let {MOUSE_HOVER, MOUSE_DRAG, MOUSE_MOVE} = constant;
+    _owner.fireEvent(MOUSE_MOVE, e);
+    if (_isMouseDown) {
+      _owner.fireEvent(MOUSE_HOVER, e);
+    } else {
+      _owner.fireEvent(MOUSE_DRAG, e);
+    }
+  }
+
+  function onMouseUp(e) {
+    let {MOUSE_UP, MOUSE_DROP} = constant;
+    _owner.fireEvent(MOUSE_UP, e);
+    if (_isMouseDown) {
+      _owner.fireEvent(MOUSE_DROP, e);
+    }
+    util.removeEventListener(document.body, 'mouseup', onMouseUp);
+  }
+
+  function bindEvent() {
+    util.addEventListener(_view, 'mousedown', onMouseDown);
+    util.addEventListener(_view, 'mousemove', onMouseMove);
+  }
+
+  function unbindEvent() {
+    util.removeEventListener(_view, 'mousedown', onMouseDown);
+    util.removeEventListener(_view, 'mousemove', onMouseMove);
+  }
+
+  return {
+
+    /**
+     * @description 绑定视图及事件
+     * @param {*} owner 
+     * @param {*} view 
+     */
+    bindView: function (owner, view) {
+      if (_view) {
+        unbindEvent();
+      }
+      _view = view;
+      _owner = owner;
+      bindEvent();
+    }
+  }
 })();
